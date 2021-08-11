bc511d226485d9e90cf1dfba1c035e617bb2e09c
linsimontao <linsimontao@gmail.com>
2021-08-11 23:01:16 +0900

    add ohen.gif

--- src/Pages/Common/Ohen.js
+++ src/Pages/Common/Ohen.js
@@ -1,9 +1,10 @@
 import OhenImg from './Ohen.png';
+import OhenGif from './Ohen.gif';
 import './Ohen.css';
 export const Ohen = ({ terrain }) => {
     return (
         <div className={terrain? "ohen-button-terrain": "ohen-button"}>
-            <img className="ohen-img" src={OhenImg} />
+            <img className="ohen-img" src={OhenGif} />
         </div>
     );
 }
