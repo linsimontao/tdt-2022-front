f61fe2150077842240347d7b2b635a2689867825
linsimontao <linsimontao@gmail.com>
2021-08-18 12:05:14 +0900

    add new img to switch map style. Upadte Ohen button gif

--- src/Pages/Common/Switch.js
+++ src/Pages/Common/Switch.js
@@ -1,11 +1,14 @@
 import './Switch.css';
+import img2D from './2d.png';
+import img3D from './3d.png';
+
 export const Switch = ({ style2D, setStyle2D }) => {
     return (
         <div 
             className={style2D? "style-switch-3D": "style-switch-2D"}
             onClick={() => setStyle2D(!style2D)}
         >
-            
+            <img className="switch-image" src={style2D? img3D: img2D}></img>
         </div>
     );
 }
